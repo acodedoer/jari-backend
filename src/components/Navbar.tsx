fc0ada33eec064a94c6372882647595935cea414
acodedoer <misbahu.s.z@gmail.com>
2023-09-12 23:20:45 +0100

    login and logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom';
+import {useCookies} from 'react-cookie';
+
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+    const logout = () => {
+        setCookies("access_token","");
+        window.localStorage.setItem("jariUserID","");
+        navigate("./auth")
+    }
     return(
         <div className="navbar">
             <Link to={"/"}>Home</Link>
             <Link to={"/create"}>Add Saying</Link>
             <Link to={"/view"}>View Sayings</Link>
-            <Link to={"/auth"}>Login/Register</Link>
-
+            {!cookies.access_token?(<Link to={"/auth"}>Login/Register</Link>):<button onClick={logout}>Logout</button>}
         </div>
     )
 }
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import axios from "axios"
+import { useCookies } from "react-cookie"
+import { useNavigate } from "react-router-dom"
 
 export const Auth = () => {
-    return <Register/>
+    return <Login/>
 }
 
 const Register = () => {
@@ -15,7 +17,8 @@ const Register = () => {
             await axios.post("http://localhost:8080/auth/register", {
                 username,
                 password
-            }).then((res)=>console.log(res))
+            }).then((res)=>alert(res.data.message))
+            .catch(()=>alert("Error connecting to Server!"))
         }
         catch(err){
             console.log(err)
@@ -24,6 +27,40 @@ const Register = () => {
 
     return(
         <AuthForm username={username} password={password} setPassword={setPassword} setUsername={setUsername} label={"Register"} onSubmit={onSubmit} />
+    )
+}
+
+const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [_, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+
+    const onSubmit = async (event:SubmitEvent) => {
+        event.preventDefault();
+        try{
+            await axios.post("http://localhost:8080/auth/login", {
+                username,
+                password
+            })
+            .then((res)=>{
+                setCookies("access_token", res && res.data.token);
+                window.localStorage.setItem("jariUserID", res && res.data.userID)
+                navigate("/")
+            })
+            .catch(({response})=>{
+                if(response.status === 401) alert(response.data.message)
+                else alert("Connection Error!")
+            })
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    return(
+        <AuthForm username={username} password={password} setPassword={setPassword} setUsername={setUsername} label={"Login"} onSubmit={onSubmit} />
     )
 }
 
